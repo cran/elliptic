0b061a923a46490da22ad8665f442c1b5ec2295b
Robin K. S. Hankin <hankin.robin@gmail.com>
2025-11-10 17:50:02 +0000

    version 1.5-1

--- inst/doc/residuetheorem.R
+++ inst/doc/residuetheorem.R
@@ -7,13 +7,13 @@ require(elliptic,quietly=TRUE)
 
 
 ###################################################
-### code chunk number 2: residuetheorem.Rnw:156-157
+### code chunk number 2: residuetheorem.Rnw:159-160
 ###################################################
 integrate.segments(exp, c(0, 1, 1+1i, 1i), close=TRUE)
 
 
 ###################################################
-### code chunk number 3: residuetheorem.Rnw:170-173
+### code chunk number 3: residuetheorem.Rnw:173-176
 ###################################################
 analytic <- exp(1)*(exp(1i)-1)
 numeric  <- integrate.segments(exp, c(1, 1+1i), close=FALSE)
@@ -21,7 +21,7 @@ c(analytic=analytic, numeric=numeric, difference=analytic-numeric)
 
 
 ###################################################
-### code chunk number 4: residuetheorem.Rnw:199-205
+### code chunk number 4: residuetheorem.Rnw:202-208
 ###################################################
 u     <- function(x){exp(pi*2i*x)}
 udash <- function(x){pi*2i * exp(pi*2i*x)}
@@ -41,7 +41,7 @@ f(z0)
 
 
 ###################################################
-### code chunk number 6: residuetheorem.Rnw:269-273
+### code chunk number 6: residuetheorem.Rnw:272-276
 ###################################################
 r <- 0.1 # radius of contour
 u <- function(x){z0 + r*exp(pi * 2i * x)}
@@ -50,7 +50,7 @@ udash <- function(x){r * pi * (0+2i) * exp(pi * 2i * x)}
 
 
 ###################################################
-### code chunk number 7: residuetheorem.Rnw:280-282
+### code chunk number 7: residuetheorem.Rnw:283-285
 ###################################################
 (val_gosper <- hypergeo_gosper(1/2, 1/3, 1/5, z0))
 abs(val_gosper - val_residue)
